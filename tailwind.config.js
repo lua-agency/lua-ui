970981399126e41966960bcd77e00ae828e54180
furkandmrblk <ibrahimdmrblk@outlook.de>
2021-09-23 20:30:34 +0200

    🚀 lua-ui setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,18 @@
 module.exports = {
 	mode: 'jit',
 	darkMode: 'class',
+<<<<<<< HEAD
 	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+=======
+	purge: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				manrope: ['Manrope'],
+			},
+>>>>>>> 213bade (🚀 lua-ui setup)
 			colors: {
 				neutral: {
 					100: '#F8F9FB',
@@ -20,5 +29,13 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
+<<<<<<< HEAD
 	plugins: [],
+=======
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms'),
+	],
+>>>>>>> 213bade (🚀 lua-ui setup)
 };
